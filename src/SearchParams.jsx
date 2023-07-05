81c7d57b9e924561e880abe1d67d9cb23302e774
DavidGuidoR <131314538+DavidGuidoR@users.noreply.github.com>
2023-07-05 15:52:43 -0600

    Remplazo  de la utilizacion de useEffect por react query en la seccion de search params para obtener un codigo mas robusto y conciso

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
-import { useState, useEffect } from "react";
+import { useState} from "react";
+import {useQuery} from "@tanstack/react-query";
 import Results from "./Results"
 import useBreedList from "./useBreedList";
+import fetchSearch from './fetchSearch';
 const ANIMALS= ['bird', 'cat', 'dog','rabbit', 'reptile'];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("");
+    const [requestParams, setRequestParams] = useState({
+        location:"",
+        animal:"",
+        breed:"",
+    });
     const [animal, setAnimal] = useState("");
     const [breed, setBreed] = useState("");
-    const [pets, setPets] = useState([]);
     const [breeds] = useBreedList(animal);
 
-    useEffect(() => {
-        requestPets();
-    }, []);
-
-    async function requestPets() {
-        const res = await fetch(
-          `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-        );
-        const json = await res.json();
-      
-        setPets(json.pets);
-      }
+    const results = useQuery (["search", requestParams], fetchSearch);
+    const pets = results?.data?.pets ?? [];
 
     return (
         // Debemos poner el nombre de la clase indicado como className debido a que class es una palabra reservada en JS.
         <div className="search-params">
             <form onSubmit={(e) => {
                 e.preventDefault();
-                requestPets();
+                const formData = new FormData(e.target);
+                const obj = {
+                    animal: formData.get("animal") ?? "",
+                    breed: formData.get("breed") ?? "",
+                    location: formData.get("location") ?? "",
+                };
+                setRequestParams(obj);
             }}>
                 <label htmlFor="location">
                     Location
                     <input
-                    onChange={(e) => setLocation(e.target.value)} 
+                    name="location"
                     id="location" 
-                    value={location} 
                     placeholder="Location" 
                     />
                 </label>
@@ -47,8 +47,7 @@ const SearchParams = () => {
                         value={animal}
                         onChange={(e) => {
                             setAnimal(e.target.value);
-                            // La inicializacion de nuevo de breed nos permite que al cambiar de opcion de animal, se borre la opcion anterior de raza.
-                            setBreed("");   
+                            // La inicializacion de nuevo de breed nos permite que al cambiar de opcion de animal, se borre la opcion anterior de raza. 
                         }}
                     >
                         <option/>
@@ -62,10 +61,7 @@ const SearchParams = () => {
                     <select
                         id="breed"
                         disabled={breeds.length ===0}
-                        value={breed}
-                        onChange={(e) => {
-                            setBreed(e.target.value);
-                        }}
+                        name = "breed"
                     >
                         <option/>
                         {breeds.map((breed) => (
