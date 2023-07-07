26cbf18815c989754df9fdb9c01f8f07f83dd8dc
DavidGuidoR <131314538+DavidGuidoR@users.noreply.github.com>
2023-07-07 15:16:40 -0600

    Adicion del context para el manejo del estado de adopcion de cada usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 import {BrowserRouter, Routes, Route } from "react-router-dom"
 import {Link} from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import {createRoot} from "react-dom/client";
+import AdoptedPetContext from './AdoptedPetContext';
 import SearchParams from "./SearchParams";
 import Details from './Details';
 
@@ -14,9 +16,11 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+    const adoptedPet = useState(null);
     return (
     <BrowserRouter>
     <QueryClientProvider client={queryClient}>
+        <AdoptedPetContext.Provider client={queryClient}>
         <header>
             <Link to="/">Adopt Me!</Link>
         </header>
@@ -25,6 +29,7 @@ const App = () => {
             <Route path="/details/:id" element={<Details />} />
             <Route path='/' element={<SearchParams />} />
         </Routes>
+        </AdoptedPetContext.Provider>
         </QueryClientProvider>
     </BrowserRouter>
     );
